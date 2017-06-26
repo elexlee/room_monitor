1654d8d687f44fb538276a1088f9fae2ba4db62a
Elex Lee <elexlee@gmail.com>
2017-06-26 16:39:36 -0700

    add a create for room notifications controller

--- app/controllers/room_notifications_controller.rb
+++ app/controllers/room_notifications_controller.rb
@@ -1,6 +1,9 @@
 class RoomNotificationsController < ApplicationController
   def create
     puts "Room Notifications Controller"
+
+    RoomNotification.create(state: params[:state], room_id: params[:room_id])
+
     render :nothing => true
   end
 end
